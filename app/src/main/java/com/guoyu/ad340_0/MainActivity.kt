33e77ffcf93b38634689667eef8425ff99ed8073
YunaoG <yunao98@outlook.com>
2020-04-27 15:03:46 -0700

    assignment2

--- app/src/main/java/com/guoyu/ad340_0/MainActivity.kt
+++ app/src/main/java/com/guoyu/ad340_0/MainActivity.kt
@@ -2,11 +2,49 @@ package com.guoyu.ad340_0
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
+        val enterButton: Button = findViewById(R.id.Button1)
+
+        enterButton.setOnClickListener {
+            val zipcode: String = zipcodeEditText.text.toString()
+            if(zipcode.length != 5 ){
+                Toast.makeText(this,R.string.zipcode_entry_error, Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,zipcode, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
